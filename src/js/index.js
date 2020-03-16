878d0e9b387d4fb8db184b35e9d07f47c281062c
zhanghua <hua.zhang@bwing.com.cn>
2020-03-16 13:17:39 +0800

    postcss

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,20 @@
 import '../css/index.less'
 import '../css/iconfont.css'
+ 
+const add = (x, y) => {
+  return x + y;
+};
 
-function add(x,y){
-    return x+y
-}
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(add(1,2))
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@
  */
 const {resolve} = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const  MiniCssExtractPlugin= require('mini-css-extract-plugin')
+const  OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+// process.env.NODE_ENV = 'development'
 module.exports = {
   entry: "./src/js/index.js",
   output: {
@@ -14,11 +17,32 @@ module.exports = {
     rules: [
       {
         test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"]
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [require("postcss-preset-env")()]
+            }
+          },
+          "less-loader"
+        ]
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [require("postcss-preset-env")()]
+            }
+          }
+        ]
       },
       {
         test: /\.(png|jpg|gif)$/,
@@ -26,20 +50,34 @@ module.exports = {
         options: {
           limit: 8 * 1024,
           name: "[hash:10].[ext]",
-          esModule:false,
-          outputPath:"images"
+          esModule: false,
+          outputPath: "images"
         }
       },
       {
-        test:/\.html/,
-        loader:'html-loader'
+        test: /\.html/,
+        loader: "html-loader"
       },
       {
         exclude: /\.(html|css|js|less|jpg|png|gif)/,
         loader: "file-loader",
         options: {
           name: "[hash:10].[ext]",
-          outputPath:"media"
+          outputPath: "media"
+        }
+      },
+      {
+        test:/\.js$/,
+        exclude:/node_modules/,
+        loader:'eslint-loader',
+        options:{}
+      },
+      {
+        test:/\.js$/,
+        exclude:/node_modules/,
+        loader:'babel-loader',
+        options:{
+          presets:['@babel/preset-env']
         }
       }
     ]
@@ -47,7 +85,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/bundle.css"
+    }),
+    new OptimizeCssAssetsWebpackPlugin( )
   ],
   devServer: {
     contentBase: resolve(__dirname, "dist"),
@@ -55,5 +97,5 @@ module.exports = {
     port: 3000,
     open: true
   },
-  mode:'development'
+  mode: "development"
 };
