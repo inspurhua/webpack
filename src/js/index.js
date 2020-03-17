99f0126e57100361ac3f57a5c2d287aaeb4f5dbf
zhanghua <hua.zhang@bwing.com.cn>
2020-03-17 13:12:26 +0800

    production

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
-import '../css/index.less'
-import '../css/iconfont.css'
+import '../css/index.less';
+import '../css/iconfont.css';
+
+function hello(x, y) {
+  return x + y;
+}
+
+console.log(hello(3, 4));
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,26 @@
  * webpack
  * npx webpack-dev-server
  */
-const {resolve} = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const  MiniCssExtractPlugin= require('mini-css-extract-plugin')
-const  OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
-// process.env.NODE_ENV = 'development'
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); //剥离单独文件
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin"); //压缩
+// process.env.NODE_ENV = 'prouction'
+//postcss-loader 使用browerslist中的那个环境,
+//loader数组从下网上执行
+//loader 先执行eslint 在执行babel-loader
+
+const CommonCssLoader = [
+  MiniCssExtractPlugin.loader,
+  "css-loader",
+  {
+    loader: "postcss-loader",
+    options: {
+      ident: "postcss",
+      plugins: () => [require("postcss-preset-env")()]
+    }
+  }
+];
 module.exports = {
   entry: "./src/js/index.js",
   output: {
@@ -17,33 +32,47 @@ module.exports = {
     rules: [
       {
         test: /\.less$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              plugins: () => [require("postcss-preset-env")()]
-            }
-          },
-          "less-loader"
-        ]
+        use: [...CommonCssLoader, "less-loader"]
       },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              plugins: () => [require("postcss-preset-env")()]
-            }
-          }
-        ]
+        use: [...CommonCssLoader]
       },
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        enforce: "pre", //先执行
+        options: {
+          fix: true
+        }
+      },
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                useBuiltIns: "usage",
+                corejs: {
+                  version: 3
+                },
+                targets: {
+                  chrome: "60",
+                  firefox: "50",
+                  ie: "9"
+                }
+              }
+            ]
+          ]
+        }
+      },
+
       {
         test: /\.(png|jpg|gif)$/,
         loader: "url-loader",
@@ -65,40 +94,21 @@ module.exports = {
           name: "[hash:10].[ext]",
           outputPath: "media"
         }
-      },
-      
-      {
-        test:/\.js$/,
-        exclude:/node_modules/,
-        loader:'babel-loader',
-        options:{
-          presets:[['@babel/preset-env',{
-            useBuiltIns:'usage',
-            corejs:{
-              version:3
-            },
-            targets:{
-              chrome:'60',
-              firefox:'50',
-              ie:'9'
-            }
-          }]]
-        }
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      minify:{
-        collapseWhitespace:true,
-        removeComments:true
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
       }
     }),
     new MiniCssExtractPlugin({
       filename: "css/bundle.css"
     }),
-    new OptimizeCssAssetsWebpackPlugin( )
+    new OptimizeCssAssetsWebpackPlugin()
   ],
   devServer: {
     contentBase: resolve(__dirname, "dist"),
